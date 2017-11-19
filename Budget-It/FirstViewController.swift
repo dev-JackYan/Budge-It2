4f5cc9a1b386e30b130c6881b60ecaf0d8a667f5
WeijaZhou <wzhou94@uwo.ca>
2017-11-18 19:37:54 -0500

    Added additional features

--- Budget-It/FirstViewController.swift
+++ Budget-It/FirstViewController.swift
@@ -9,56 +9,71 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
- 
+    
     @IBOutlet weak var Text1: UITextField!
-
     @IBOutlet weak var Text2: UITextField!
-
     @IBOutlet weak var Text3: UITextField!
     @IBOutlet weak var Text4: UITextField!
     @IBOutlet weak var Text5: UITextField!
-    @IBAction func NextButton(_ sender: Any) {
+       @IBAction func NextButton(_ sender: Any) {
         self.view.endEditing(true)
         
-        if(isKeyPresentInUserDefaults(key: "Text1")){
-            UserDefaults.standard.removeObject(forKey: "Text1")
-            UserDefaults.standard.set(Text1.text,forKey: "Text1")
+        if(isKeyPresentInUserDefaults(key: "TotalExpense1")){
+            UserDefaults.standard.removeObject(forKey: "TotalExpense1")
+            UserDefaults.standard.set(Text1.text,forKey: "TotalExpense1")
+            UserDefaults.standard.removeObject(forKey: "CurrentExpense1")
+            UserDefaults.standard.set(Text1.text,forKey: "CurrentExpense1")
         }
         else{
-            UserDefaults.standard.set(Text1.text,forKey: "Text1")
+            UserDefaults.standard.set(Text1.text,forKey: "TotalExpense1")
+            UserDefaults.standard.set(Text1.text,forKey: "CurrentExpense1")
+            
         }
-        if(isKeyPresentInUserDefaults(key: "Text2")){
-            UserDefaults.standard.removeObject(forKey: "Tex2")
-            UserDefaults.standard.set(Text2.text,forKey: "Text2")
+        if(isKeyPresentInUserDefaults(key: "TotalExpense2")){
+            UserDefaults.standard.removeObject(forKey: "TotalExpense2")
+            UserDefaults.standard.set(Text2.text,forKey: "TotalExpense2")
+            UserDefaults.standard.removeObject(forKey: "CurrentExpense2")
+            UserDefaults.standard.set(Text2.text,forKey: "CurrentExpense2")
         }
         else{
-            UserDefaults.standard.set(Text2.text,forKey: "Text2")
+            UserDefaults.standard.set(Text2.text,forKey: "TotalExpense2")
+            UserDefaults.standard.set(Text2.text,forKey: "CurrentExpense2")
+            
         }
         
-        if(isKeyPresentInUserDefaults(key: "Text3")){
-            UserDefaults.standard.removeObject(forKey: "Text3")
-            UserDefaults.standard.set(Text3.text,forKey: "Text3")
+        if(isKeyPresentInUserDefaults(key: "TotalExpense3")){
+            UserDefaults.standard.removeObject(forKey: "TotalExpense3")
+            UserDefaults.standard.set(Text3.text,forKey: "TotalExpense3")
+            UserDefaults.standard.removeObject(forKey: "CurrentExpense3")
+            UserDefaults.standard.set(Text3.text,forKey:"CurrentExpense3") }
+        else{
+            UserDefaults.standard.set(Text3.text,forKey: "TotalExpense3")
+            UserDefaults.standard.set(Text3.text,forKey: "CurrentExpense3")
+            
+        }
+        if(isKeyPresentInUserDefaults(key: "TotalExpense4")){
+            UserDefaults.standard.removeObject(forKey: "TotalExpense4")
+            UserDefaults.standard.set(Text4.text,forKey: "TotalExpense4")
+            UserDefaults.standard.removeObject(forKey: "CurrentExpense4")
+            UserDefaults.standard.set(Text4.text,forKey:"CurrentExpense4")
         }
         else{
-            UserDefaults.standard.set(Text3.text,forKey: "Text3")
+            UserDefaults.standard.set(Text4.text,forKey: "TotalExpense4")
+            UserDefaults.standard.set(Text4.text,forKey: "CurrentExpense4")
         }
-        if(isKeyPresentInUserDefaults(key: "Text4")){
-            UserDefaults.standard.removeObject(forKey: "Text4")
-            UserDefaults.standard.set(Text4.text,forKey: "Text4")
-        }
+        if(isKeyPresentInUserDefaults(key: "TotalExpense5")){
+            UserDefaults.standard.removeObject(forKey: "TotalExpense5")
+            UserDefaults.standard.set(Text5.text,forKey: "TotalExpense5")
+            UserDefaults.standard.removeObject(forKey: "CurrentExpense5")
+            UserDefaults.standard.set(Text5.text,forKey:"CurrentExpense5")}
         else{
-            UserDefaults.standard.set(Text4.text,forKey: "Text4")
+            UserDefaults.standard.set(Text5.text,forKey: "TotalExpense5")
+            UserDefaults.standard.set(Text5.text,forKey: "CurrentExpense5")
         }
-        if(isKeyPresentInUserDefaults(key: "Text5")){
-            UserDefaults.standard.removeObject(forKey: "Text5")
-            UserDefaults.standard.set(Text5.text,forKey: "Text5")
-        }
-        else{
-            UserDefaults.standard.set(Text5.text,forKey: "Text5")
-        }
+        
+        
         
     }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
