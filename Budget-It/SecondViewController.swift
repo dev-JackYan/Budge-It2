16159ea234bcbae20a38bae745c7ce951ac8fe27
WeijaZhou <wzhou94@uwo.ca>
2017-11-18 20:38:02 -0500

    merged code from Weija and Frank

--- Budget-It/SecondViewController.swift
+++ Budget-It/SecondViewController.swift
@@ -59,6 +59,45 @@ class SecondViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        
+        
+        if let a = UserDefaults.standard.object(forKey: "TotalExpense1") as? String{
+            Text1.text = a
+        }
+        if let b = UserDefaults.standard.object(forKey: "CurrentExpense1") as? String{
+            Text6.text = b
+        }
+        if let c = UserDefaults.standard.object(forKey: "TotalExpense2") as? String{
+            Text2.text = c
+        }
+        if let d = UserDefaults.standard.object(forKey: "CurrentExpense2") as? String{
+            Text7.text = d
+        }
+        if let e = UserDefaults.standard.object(forKey: "TotalExpense3") as? String{
+            Text3.text = e
+        }
+        if let f = UserDefaults.standard.object(forKey: "CurrentExpense3") as? String{
+            Text8.text = f
+        }
+        if let g = UserDefaults.standard.object(forKey: "TotalExpense4") as? String{
+            Text4.text = g
+        }
+        if let h = UserDefaults.standard.object(forKey: "CurrentExpense4") as? String{
+            Text9.text = h
+        }
+        if let i = UserDefaults.standard.object(forKey: "TotalExpense5") as? String{
+            Text5.text = i
+        }
+        if let j = UserDefaults.standard.object(forKey: "CurrentExpense5") as? String{
+            Text10.text = j
+        }
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Budget-It/ThirdViewController.swift
@@ -0,0 +1,91 @@
+//
+//  SecondViewController.swift
+//  Budget-It
+//
+//  Created by Weija Zhou on 2017-11-18.
+//  Copyright © 2017 Weija Zhou. All rights reserved.
+//
+import UIKit
+
+class ThirdViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    
+    @IBOutlet weak var PickerView: UIPickerView!
+    
+    @IBOutlet weak var InputField: UITextField!
+    @IBOutlet weak var Label1: UILabel!
+    
+    var currentCategory: String = "Fixed Expense"
+  
+    
+    
+    let categories = ["Fixed Expenses", "Food", "Loans", "Transportation", "Personal Expenses"]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row : Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent row : Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row : Int, inComponent component: Int)  {
+        
+        currentCategory = categories[row]
+    }
+    
+    
+    @IBAction func EnterButton(_ sender: Any) {
+        
+        var textNumber: String = ""
+        if (currentCategory == "Fixed Expense"){
+            textNumber = "CurrentExpense1"
+        }
+        else if (currentCategory == "Food"){
+            textNumber = "CurrentExpense2"
+        }
+        else if (currentCategory == "Loans"){
+            textNumber = "CurrentExpense3"
+        }
+        else if (currentCategory == "Transportation"){
+            textNumber = "CurrentExpense4"
+        }
+        else if (currentCategory == "Personal Expenses"){
+            textNumber = "CurrentExpense5"
+        }
+        let x = UserDefaults.standard.object(forKey: textNumber) as? String
+        print(textNumber)
+        print(x)
+        let currentExpense =  Int(x!)
+        print (currentExpense)
+        var text = InputField.text
+        var int_Text = Int(text!)
+        let newExpense = currentExpense! - int_Text!
+        let string_NewExpense = String(newExpense)
+        UserDefaults.standard.removeObject(forKey: textNumber)
+        UserDefaults.standard.set(string_NewExpense,forKey: textNumber)
+        print(string_NewExpense)
+        //        self.performSegue(withIdentifier: "Segue1", sender: self)
+        
+
+    }
+    
+        override func viewDidLoad() {
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //    override func viewDidDisappear(_ animated: Bool) {
+    //
+    //    }
+    //
+    //    
+    
+    
+    
+}
